152a73f78163d5e71cfd4df8044c2ae5c5ca6fee
İrem Kabaoğlu <iremkabaoglu1@gmail.com>
2025-02-06 17:39:54 +0300

    Sql Bağlantısı Oluşturuldu

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ToDo_WebAplication.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add EF Core DI
+builder.Services.AddDbContext<ToDoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoContext"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
